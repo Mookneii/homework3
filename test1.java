1e10f2921c00475ab75ad5336f67f401c52acc44
Mony <monyjk576@gmail.com>
2025-12-02 09:03:33 +0700

    new

--- test1.java
+++ test1.java
@@ -1,8 +1,9 @@
-public class Test1 {
+public class test1 {
 
     public static void main(String args[]) {
         System.out.println("Welcome to class");
 	System.out.println("This is our changable codes that ");
+    System.out.println("B");
 
         for (int i = 0; i < 1000; i++) {
             System.out.print(i + " ");
